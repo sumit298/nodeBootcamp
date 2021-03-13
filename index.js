afc89ef806ffe285e51f89538581cbce6a9c67c0
sumitsssss <sumitsinhassss@outlook.com>
2021-03-13 17:46:48 +0530

    Initialized Express app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+const port = 5000 || process.env.PORT;
+
+app.listen(port, () =>
+  console.log(
+    `Server up and running in ${process.env.NODE_ENV} mode at port ${port}.`
+  )
+);
